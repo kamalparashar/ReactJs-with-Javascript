0e10133b11637c6719073de57871e8abf1e95f95
Kamal Parashar <kamalparasharp1123@gmail.com>
2024-03-08 19:50:43 +0530

    contextAPI ThemeChange with contextAPI

---
+++ 09themechanger/src/context/theme.js
@@ -0,0 +1,16 @@
+import {createContext, useContext} from 'react'
+
+export const ThemeContext = createContext({
+    themeMode:"light",
+    lightTheme: () => {},
+    darkTheme: () => {}
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+//custom Hook
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
+
+
